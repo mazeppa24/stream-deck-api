9f949a5ce4750505d67ed747114673dd48ee1d72
mazeppa <mazeppa@nym.hush.com>
2019-02-09 19:38:31 +0100

    - don't ask why, but it looks ok

--- index.js
+++ index.js
@@ -4,21 +4,28 @@ let streamDeck = streamDeckApi.getStreamDeck()
 streamDeck.reset()
 
 
-streamDeck.drawText('TCN ', 'white', 1,'black');
-streamDeck.drawText(':TCN ', 'green',2,'black');
+streamDeck.drawText('TCN', 'black', 1,'white');
+streamDeck.drawText('AP', 'black',2,'white');
+streamDeck.drawText('HSEL', 'black',3,'white');
+streamDeck.drawText(':HSEL', 'black',4,'white');
 
-streamDeck.drawText('HSEL', 'green',3,'black');
-streamDeck.drawText(':HSEL', 'green',4,'black');
+streamDeck.drawText('12', 'black',5,'white');
 
-streamDeck.drawText('TCN', 'white',6,'black');
-streamDeck.drawText('AP', 'white',7,'black');
+streamDeck.drawText('TCN', 'white', 6,'black');
+streamDeck.drawText('LP', 'white',7,'black');
+streamDeck.drawText('HSEL', 'white',8,'black');
+streamDeck.drawText(':HSEL', 'white',9,'black');
+
+streamDeck.drawText('23:30', 'white',10,'black');
 
 
-streamDeck.drawText('UFC', 'black',8,'white');
-streamDeck.drawText('AP', 'black',9,'white');
 
+streamDeck.drawText('TCN', 'green', 11,'black');
+streamDeck.drawText('LP', 'green',12,'black');
+streamDeck.drawText('HSEL', 'green',13,'black');
+streamDeck.drawText(':HSEL', 'green',14,'black');
 
-streamDeck.drawText('TCN', 'white',11,'black');
+streamDeck.drawText('A', 'green',15,'black');
 
 /*
 streamDeck.drawTextFile('lakalala', 2);
