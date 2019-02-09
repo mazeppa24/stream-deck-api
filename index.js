ec0cfac4d1318bd3d98a8d2a512a82568dc73405
mazeppa <dave24vecora>
2019-02-09 16:48:54 +0100

    - drawText to button with jimp and text2png

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const streamDeckApi = require('./lib/stream-deck-api')
 let streamDeck = streamDeckApi.getStreamDeck()
 streamDeck.reset()
 
+
+streamDeck.drawText('TCN ', 'green', 'black',1 , 200);
+streamDeck.drawText(':TCN ', 'green','black', 2, 200);
+
+streamDeck.drawText('HSEL', 'green','black', 6, 200);
+streamDeck.drawText(':HSEL', 'green','black', 7, 200);
+
+/*
+streamDeck.drawTextFile('lakalala', 2);
 streamDeck.drawImageFile('./images/nyancat.png', 1)
 
 streamDeck.on('down', (buttonNumber) => {
@@ -16,7 +25,7 @@ streamDeck.on('up', (buttonNumber) => {
 streamDeck.on('state', (state) => {
   console.log(state) // eslint-disable-line no-console
 })
-
+*/
 process.on('SIGINT', () => {
   streamDeck.reset()
   process.exit()
